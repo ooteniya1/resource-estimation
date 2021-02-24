4308224b94cafb58544d91869b502964b6b2a380
Olu Oteniya <ooteniya@redhat.com>
2021-02-23 18:59:54 -0700

    initial

---
+++ tekton-pipeline/build-deploy-todo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#create the resources if not existing
+oc apply -f res/*.yaml
+
+# run ci tasks
+oc apply -f ci/*.yaml
+oc apply -f todo-pipeline.yaml
+
+tkn pipeline start todo-deploy-pipeline \
+ --resource="appSource=todo-app-source" \
+ --resource="appImage=todo-app-image" \
+ --param="contextDir=springboot" \
+ --param="manifest_dir=k8s" \
+ --serviceaccount='pipeline'\
+ --showlog
+
+# tkn task start  build-app \
+#   --inputresource='source=todo-app-source' \
+#   --outputresource='builtImage=todo-app-image' \
+#   --param contextDir='springboot' \
+#   --showlog
+
+# run cd tasks
+oc apply -f cd/*.yaml
---
+++ todo-spring-quarkus/src/main/java/io/quarkus/todospringquarkus/TodoController.java
@@ -0,0 +1,53 @@
+package io.quarkus.todospringquarkus;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @GetMapping
+    public List<TodoEntity> findAll() {
+        System.out.println("Printing mesage from: ");
+        return todoRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+    }
+ 
+    @GetMapping("/{id}")
+    public TodoEntity findById(@PathVariable("id") Long id) {
+        return todoRepository.findById(id).get();
+    }
+
+    @PutMapping
+    @Transactional
+    public void update(@RequestBody TodoEntity resource) {
+        todoRepository.save(resource);
+    }
+ 
+    @PostMapping
+    @Transactional
+    public TodoEntity create(@RequestBody TodoEntity resource) {
+        return todoRepository.save(resource);
+    }
+ 
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void delete(@PathVariable("id") Long id) {
+        todoRepository.deleteById(id);
+    }
+}
