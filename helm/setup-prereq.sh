5ad700e0424ceee8c57c2690df0af75cfa455fae
Olu Oteniya <ooteniya@redhat.com>
2021-03-30 10:56:33 -0600

    added helm chart to create resource-estimation ns

--- helm/setup-prereq.sh
+++ helm/setup-prereq.sh
@@ -3,12 +3,16 @@
 set -e
 
 echo ""
+# install helms charts that will create the resource-estimation namespace
+echo "Creating resource-estimation namespace"
+helm upgrade --install resource-estimation-ns resource-estimation-ns
+
+echo ""
 # install helms charts that will initialize the environment 
 echo "Installing openshift-pipelines operator"
 helm upgrade --install openshift-pipelines openshift-pipelines
 
 echo ""
-
 echo "Installing postgresql Operator" 
 helm upgrade --install postgresql postgresql
 #oc apply -f ../todo-spring-quarkus/k8s/postgresql-instance.yaml
